5374201e05e184094950204f790e0356012f170f
Louis Sutton <louis.sutton.iii@gmail.com>
2020-01-24 05:05:26 -0800

    Got current position to work

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,38 +1,45 @@
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var google;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements AfterViewInit{
   
-    title = 'angular-gmap';
-    @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
-    map: google.maps.Map;
-    lat = 40.73061;
-    lng = -73.935242;
+  constructor(private geolocation: Geolocation) {}
+  title = 'angular-gmap';
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  
 
-    coordinates = new google.maps.LatLng(this.lat, this.lng);
+  LatLng = new google.maps.LatLng(this.lat, this.lng);
+  
+  mapOptions: google.maps.MapOptions = {
+   center: this.LatLng,
+   zoom: 8
+   
+  };
 
-    mapOptions: google.maps.MapOptions = {
-     center: this.coordinates,
-     zoom: 8
-    };
+  marker = new google.maps.Marker({
+    position: this.LatLng,
+    map: this.map,
+  });
 
-    marker = new google.maps.Marker({
-      position: this.coordinates,
-      map: this.map,
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.geolocation.getCurrentPosition().then(resp => {
+      let LatLng = new google.maps.LatLng(resp.coords.latitude,  resp.coords.longitude)
+      this.map.setCenter(LatLng);
+      this.marker.setMap(LatLng);
     });
-
-    ngAfterViewInit() {
-      this.mapInitializer();
-    }
-
-    mapInitializer() {
-      this.map = new google.maps.Map(this.gmap.nativeElement, 
-      this.mapOptions);
-      this.marker.setMap(this.map);
-    }
-
-   }
+    
+    
+  }
+ }
